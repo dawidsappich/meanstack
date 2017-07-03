5944ef74ac6f19ba407f36d46ebf35c42e8b6c0b
dawidsappich <dawidsappich@gmail.com>
2017-07-03 19:03:08 +0200

    add bare validation and validation messages in view

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  debug: boolean = false;
+
+  constructor(private _fb: FormBuilder) {
+    this.createForm();
+  }
 
   ngOnInit() {
+  }
+
+  private createForm(): void {
+    this.form = this._fb.group({
+      username: ['', Validators.required ],
+      email: ['', Validators.required ],
+      password: ['', Validators.required ],
+      confirm: ['', Validators.required ]
+    })
+  }
+
+  onSubmitForm() {
+    console.log(this.form.controls);
   }
 
 }

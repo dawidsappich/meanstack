59456bea12321b3df300c6a3cbdbe9cadb94a82c
dawidsappich <dawidsappich@gmail.com>
2017-07-03 19:53:41 +0200

    add reative form validation

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -20,15 +20,45 @@ export class RegisterComponent implements OnInit {
 
   private createForm(): void {
     this.form = this._fb.group({
-      username: ['', Validators.required ],
-      email: ['', Validators.required ],
-      password: ['', Validators.required ],
+      username: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+        this.validateUserName
+      ])],
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(50),
+        this.validateEmail
+      ])],
+      password: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20)
+      ])],
       confirm: ['', Validators.required ]
-    })
+    }, { validator: this.matchingPasswords('password', 'confirm') })
   }
 
   onSubmitForm() {
     console.log(this.form.controls);
+  }
+
+  validateEmail(controls) {
+    const regex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    return (regex.test(controls.value)) ? null : { notValidEmail: true };
+  }
+
+  validateUserName(controls) {
+    const regex = new RegExp(/^[a-zA-Z0-9]+$/);
+    return (regex.test(controls.value)) ? null : { notValidUserName: true };
+  }
+
+  matchingPasswords(password, confirm) {
+    return (group: FormGroup) => {
+      return (group.controls[password].value === group.controls[confirm].value) ? null : { notMatchingPasswords: true };
+    }
   }
 
 }
